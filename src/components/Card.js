39481ef97286e00bd72b5d51a2891ebc7e7a13d5
dimtirisafendras <dimitris.afendras@gmail.com>
2020-02-12 17:30:46 +0200

    resulted skeleton

--- src/components/Card.js
+++ src/components/Card.js
@@ -185,7 +185,45 @@ export const Card = () =>
 		Commit progress on footer's skeleton
 		<div className='Separator'/>
 
+		<div className='Separator'/>
+		The resulted skeleton
+		<div className='Separator'/>
 
+		<div className='Card'>
+			<div className='Header'>
+				<div className='AccountInfoContainer'>
+					<div className='Icon'></div>
+					<div className='InfoContainer'>
+						<div className='UserName'></div>
+						<div className='AtUserName'></div>
+					</div>
+				</div>
+				<div className='Logo'></div>
+			</div>
+			<div className='SubHeaderContainer'>
+				<div className='Text'></div>
+				<div className='Link'></div>
+			</div>
+			<div className='ContentContainer'>
+				<div className='Image'></div>
+				<div className='ContentInfoContainer'>
+					<div className='ContentHeader'></div>
+					<div className='ContentText'></div>
+					<div className='ContentLink'></div>
+				</div>
+			</div>
+			<div className='SubContentContainer'>
+				<div className='MetadataContainer'>
+					<div className='Likes'></div>
+					<div className='Date'></div>
+				</div>
+				<div className='InfoIcon'></div>
+			</div>
+			<div className='Footer'>
+				<div className='ExtraInfo'></div>
+				<div className='NextBTN'></div>
+			</div>
+		</div>
 
 	</>;
 
