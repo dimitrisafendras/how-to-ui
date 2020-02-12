4b71b2d8a1965093f984208182608c845094e23e
dimtirisafendras <dimitris.afendras@gmail.com>
2020-02-12 13:40:01 +0200

    basic card structure

---
+++ src/components/Card.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Card.css';
+
+
+export const Card = () =>
+	<>
+		<div className='Card'>
+		</div>
+
+		<div className='Card'>
+			<div className='Header'></div>
+		</div>
+
+		<div className='Card'>
+			<div className='Header'></div>
+			<div className='SubHeader'></div>
+		</div>
+
+		<div className='Card'>
+			<div className='Header'></div>
+			<div className='SubHeader'></div>
+			<div className='Content'></div>
+		</div>
+
+		<div className='Card'>
+			<div className='Header'></div>
+			<div className='SubHeader'></div>
+			<div className='Content'></div>
+			<div className='SubContent'></div>
+		</div>
+
+		<div className='Card'>
+			<div className='Header'></div>
+			<div className='SubHeader'></div>
+			<div className='Content'></div>
+			<div className='SubContent'></div>
+			<div className='Footer'></div>
+		</div>
+
+	</>;
+
+
+
+
+
+
+
