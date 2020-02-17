19b1decacf6b66a68149736f1f62a06c0daa2ddd
dimtirisafendras <dimitris.afendras@gmail.com>
2020-02-17 14:34:19 +0200

    finished content

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,7 @@ import Heart from '../assets/heart.svg';
 import Info from '../assets/info.svg';
 import Logo from '../assets/logo.svg';
 import Twitter from '../assets/twitter.svg';
+import BG from '../assets/milky-way.jpg';
 
 export const Card = () =>
 	<>
@@ -392,6 +393,129 @@ export const Card = () =>
 				<img src={Direction} className='NextBTN2' />
 			</div>
 		</div>
+
+		<Separator text={'\t\tReplace Content\'s placeholders with actual ui\n'}/>
+
+		<div className='ContentContainer'>
+			<div className='Image'></div>
+			<div className='ContentInfoContainer'>
+				<div className='ContentHeader'></div>
+				<div className='ContentText'></div>
+				<div className='ContentLink'></div>
+			</div>
+		</div>
+
+		<Separator/>
+
+		<div className='ContentContainer1'>
+			<div className='Image1'></div>
+			<div className='ContentInfoContainer1'>
+				<div className='ContentHeader1'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+				<div className='ContentText1'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+				<div className='ContentLink1'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+			</div>
+		</div>
+
+		<Separator/>
+
+		<div className='ContentContainer2'>
+			<img src={BG} className='Image2'></img>
+			<div className='ContentInfoContainer2'>
+				<div className='ContentHeader2'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+				<div className='ContentText2'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+				<div className='ContentLink2'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+			</div>
+		</div>
+
+		<Separator/>
+
+		<div className='ContentContainer3'>
+			<img src={BG} className='Image3'></img>
+			<div className='ContentInfoContainer3'>
+				<div className='ContentHeader3'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+				<div className='ContentText3'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+				<div className='ContentLink3'>
+					It is a long established fact that a reader will be distracted by the
+				</div>
+			</div>
+		</div>
+
+		<Separator text={'Commit Content'}/>
+
+		<div className='Card'>
+			<div className='Header3'>
+				<div className='AccountInfoContainer3'>
+					<img src={Logo} className='Icon3'/>
+					<div className='InfoContainer3'>
+						<div className='UserName3'>
+							Lorem Ipsum
+						</div>
+						<div className='AtUserName3'>
+							@Lorem Ipsum
+						</div>
+					</div>
+				</div>
+				<img src={Twitter} className='Logo3' />
+			</div>
+			<div className='SubHeaderContainer2'>
+				<div className='Text2'>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+					Lorem Ipsum has been the industry'sLorem Ipsum has been the industry's
+				</div>
+				<div className='Link2'>
+					Lorem Ipsum has been the industry'sLorem Ipsum has been the industry's
+				</div>
+			</div>
+			<div className='ContentContainer3'>
+				<img src={BG} className='Image3'></img>
+				<div className='ContentInfoContainer3'>
+					<div className='ContentHeader3'>
+						It is a long established fact that a reader will be distracted by the
+					</div>
+					<div className='ContentText3'>
+						It is a long established fact that a reader will be distracted by the
+					</div>
+					<div className='ContentLink3'>
+						It is a long established fact that a reader will be distracted by the
+					</div>
+				</div>
+			</div>
+			<div className='SubContentContainer2'>
+				<div className='MetadataContainer2'>
+					<img src={Heart} className='Likes2'/>
+					<div className='Date2'>
+						Lorem Ipsum has been
+					</div>
+				</div>
+				<img src={Info} className='InfoIcon2'/>
+			</div>
+			<div className='Footer2'>
+				<div className='ExtraInfoContainer'>
+					<img src={Comment} className='ExtraInfoIcon' />
+					<div className='ExtraInfoTxt'>
+						Lorem Ipsum is simply dummy text of the
+					</div>
+				</div>
+				<img src={Direction} className='NextBTN2' />
+			</div>
+		</div>
+
 
 	</>;
 
